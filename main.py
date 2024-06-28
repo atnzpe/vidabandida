8f26d2e6011479d8b73bbdb0b9505694236bfb0c
Gleyson Atanázio <gleysonasilva@gmail.com>
2024-06-28 18:42:42 -0300

    refactor: Aprimorado o arquivo main.py
    - Adicionados comentários explicativos.
    - Renomeada a variável `app` para `oficina_app`.
    - Melhorada a organização do código.

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-
-import flet as ft #inclui
+import flet as ft
 import threading
-from flet import Dropdown, dropdown  # Importa Dropdown e dropdown
 
 from oficina_app import OficinaApp, processar_fila_db
 from utils import criar_pastas
 
-
 def main(page: ft.Page):
-    page.Title = "Oficina Guarulhos Teste cadastro carro"
-    page.theme_mode = ft.ThemeMode.DARK
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    app = OficinaApp(page)
-    
-    
+    """Função principal da aplicação Flet."""
 
-    # Inscreva-se para receber mensagens da thread do banco de dados
-    page.pubsub.subscribe(app._on_message)
+    # Configuração da página
+    page.title = "Oficina Guarulhos"  # Título da página
+    page.theme_mode = ft.ThemeMode.DARK  # Tema escuro
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER  # Alinhamento centralizado
 
-    # Criar as pastas necessárias
+    # Cria a estrutura de diretórios
     criar_pastas(".")
-    
-    # Inicie a thread para processar a fila
-    thread_db = threading.Thread(target=processar_fila_db, args=(page,), daemon=True)
-    thread_db.start()
 
+    # Inicializa a aplicação
+    oficina_app = OficinaApp(page)
+
+    # Gerenciamento da thread do banco de dados
+    page.pubsub.subscribe(oficina_app._on_message)  # Inscreve-se para receber mensagens da thread
+    thread_db = threading.Thread(
+        target=processar_fila_db, args=(page,), daemon=True
+    )  # Cria a thread
+    thread_db.start()  # Inicia a thread
+
+    # Inicialização da interface
     page.update()
-    page.add(app.build())
+    page.add(oficina_app.build())
 
 ft.app(target=main)
